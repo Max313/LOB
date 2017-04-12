fe4c70352012c5d301a4ec50dda2720c513e4a08
Stina Schick <schick@tmis.de>
2017-04-12 15:13:22 +0200

    commit

---
+++ app/src/main/java/com/example/lammel/lob/Staerkeinsel.java
@@ -0,0 +1,13 @@
+package com.example.lammel.lob;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Staerkeinsel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_staerkeinsel);
+    }
+}
