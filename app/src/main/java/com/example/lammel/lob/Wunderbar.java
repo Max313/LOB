5169fc491a8db9198cabc7394aa5cf2768a633d4
Stina Schick <schick@tmis.de>
2017-04-12 19:29:50 +0200

    dimen for normalContent

--- app/src/main/java/com/example/lammel/lob/Wunderbar.java
+++ app/src/main/java/com/example/lammel/lob/Wunderbar.java
@@ -1,13 +1,20 @@
 package com.example.lammel.lob;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class Wunderbar extends AppCompatActivity {
+public class Wunderbar extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wunderbar);
+    }
+
+    @Override
+    public void onClick(View view) {
+        startActivity(new Intent(this, Staerkeinsel.class));
     }
 }
