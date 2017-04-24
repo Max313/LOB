b5529f3ab22cd34676aecf212ad42559e5fcf953
Stina Schick <schick@tmis.de>
2017-04-24 13:19:36 +0200

    bugfix

--- app/src/main/java/com/example/lammel/lob/Level4InselDesSehenden.java
+++ app/src/main/java/com/example/lammel/lob/Level4InselDesSehenden.java
@@ -4,23 +4,25 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class Level4InselDesSehenden extends AppCompatActivity implements View.OnClickListener{
 
-    private ImageView eye;
-
+    private Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level4_insel_des_sehenden);
-        this.setTitle("LOB - Konsolidierung");
+        this.setTitle("LOB - Insel des Sehenden");
+
+        start = (Button) findViewById(R.id.fragenStart_Button);
+        start.setOnClickListener(this);
 
 
 
-        eye = (ImageView) findViewById(R.id.Auge_imageView);
-        eye.setOnClickListener(this);
+
 
 
     }
