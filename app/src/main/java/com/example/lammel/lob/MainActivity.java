1e5bfe3531a9fa927d9daa91ab79a01609767e6e
Stina Schick <schick@tmis.de>
2017-04-26 17:35:55 +0200

    tool bar

--- app/src/main/java/com/example/lammel/lob/MainActivity.java
+++ app/src/main/java/com/example/lammel/lob/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.lammel.lob;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,10 +15,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(myToolbar);
         this.setTitle("LOB - Willkommen");
         Button start_button = (Button) findViewById(R.id.start_button);
         start_button.setOnClickListener(this);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id == R.id.activity_main){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     public void onClick(View v) {
